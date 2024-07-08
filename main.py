d7c022affb91b363ff40989a7de8e5fdad162afc
Alina Baranova <alina_nemo@mail.ru>
2024-07-08 21:33:21 +0300

    Change output for user

--- main.py
+++ main.py
@@ -20,21 +20,26 @@ while True:
 5. Ввести ключевое слово для поиска
 6. Выход
 """)
-    user_answer = int(input('Введи цифру: '))
+    user_answer = input('Введи цифру: ')
     db = DBManager(db_name)
-    if user_answer in range(1,7):
-        if user_answer == 1:
+    if user_answer in ['1', '2', '3', '4', '5', '6']:
+        if user_answer == '1':
             print(json.dumps(db.get_companies_and_vacancies_count(), ensure_ascii=False, indent=4))
-        if user_answer == 2:
+        if user_answer == '2':
             print(json.dumps(db.get_all_vacancies(), ensure_ascii=False, indent=4))
-        if user_answer == 3:
+        if user_answer == '3':
             print(db.get_avg_salary())
-        if user_answer == 4:
+        if user_answer == '4':
             print(db.get_vacancies_with_higher_salary())
-        if user_answer == 5:
+        if user_answer == '5':
             user_word = input('Введи слово: ')
-            print(json.dumps(db.get_vacancies_with_keyword(user_word), ensure_ascii=False, indent=4))
-        if user_answer == 6:
+            json_list = db.get_vacancies_with_keyword(user_word)
+            if len(json_list) == 0:
+                print('Таких вакансий нет')
+            else:
+                print(json.dumps(json_list, ensure_ascii=False, indent=4))
+            print('Поиск завершен')
+        if user_answer == '6':
             break
     else:
         print("Такой цифры нет, попробуй еще раз")
