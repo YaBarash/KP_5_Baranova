9d46f841ed800cb8b0eb4a25426bd55375d8e79f
Alina Baranova <alina_nemo@mail.ru>
2024-07-08 21:39:22 +0300

    Add trailing whitespace

--- src/db_manager.py
+++ src/db_manager.py
@@ -27,7 +27,7 @@ class DBManager:
     def get_companies_and_vacancies_count(self):
         '''получает список всех компаний и количество вакансий у каждой компании'''
         return self.__execute_query("""
-        SELECT employers.name, COUNT(*) AS count_vacancies 
+        SELECT employers.name, COUNT(*) AS count_vacancies
         FROM employers
         LEFT JOIN vacancies ON vacancies.employer=employers.id
         GROUP BY employers.name
@@ -36,18 +36,18 @@ class DBManager:
     def get_all_vacancies(self):
         '''получает список всех вакансий с указанием названия компании, названия вакансии,зп и ссылки на вакансию'''
         return self.__execute_query("""
-        SELECT employers.name AS company_name, vacancies.name AS vac_name, 
-        vacancies.url, vacancies.salary_from, vacancies.salary_to 
-        FROM employers 
+        SELECT employers.name AS company_name, vacancies.name AS vac_name,
+        vacancies.url, vacancies.salary_from, vacancies.salary_to
+        FROM employers
         LEFT JOIN vacancies ON vacancies.employer=employers.id
         """)
 
     def get_avg_salary(self):
         '''получает среднюю зарплату по вакансии'''
         return self.__execute_query("""
-        SELECT employer, name, round(avg(salary_from),2) AS salary_avg 
-        FROM vacancies 
-        GROUP BY name, employer 
+        SELECT employer, name, round(avg(salary_from),2) AS salary_avg
+        FROM vacancies
+        GROUP BY name, employer
         ORDER BY round(avg(salary_from),2) DESC, name
         """)
 
@@ -55,7 +55,7 @@ class DBManager:
         '''получает список всех вакансий, у которых зарплата выше средней по всем вакансиям'''
         return self.__execute_query("""
         SELECT name, round(salary_from,2) AS salary_round
-        FROM vacancies 
+        FROM vacancies
         WHERE  salary_from >= (select round(avg(salary_from),2) AS salary_from from vacancies)
         ORDER BY round(salary_from,2) DESC
         """)
@@ -64,14 +64,12 @@ class DBManager:
         '''получает список всех вакансий, в названии которых содержатся переданные в метод слова'''
         new_list_vacancies = []
         vacancies = self.__execute_query("""
-        SELECT employers.name AS company_name, vacancies.name AS vac_name, 
-        vacancies.url, vacancies.salary_from, vacancies.salary_to 
-        FROM employers 
+        SELECT employers.name AS company_name, vacancies.name AS vac_name,
+        vacancies.url, vacancies.salary_from, vacancies.salary_to
+        FROM employers
         LEFT JOIN vacancies ON vacancies.employer=employers.id
         """)
         for vac in vacancies:
             if keyword.lower() in vac[1].lower():
                 new_list_vacancies.append(vac)
         return new_list_vacancies
-
-
