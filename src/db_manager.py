656954f1dd0af96120c88490ff3331ffcc9eb390
Alina Baranova <alina_nemo@mail.ru>
2024-07-09 18:11:38 +0300

    Change methods for search vacancies with keword and add method for work with general avg salary

--- src/db_manager.py
+++ src/db_manager.py
@@ -43,12 +43,22 @@ class DBManager:
         """)
 
     def get_avg_salary(self):
-        '''получает среднюю зарплату по вакансии'''
+        '''получает среднюю зарплату по каждой вакансии'''
         return self.__execute_query("""
         SELECT employer, name, round(avg(salary_from),2) AS salary_avg
         FROM vacancies
         GROUP BY name, employer
         ORDER BY round(avg(salary_from),2) DESC, name
+        """)
+
+    def get_general_avg_salary(self):
+        """получает среднюю зарплату по ВСЕМ вакансиями, где:
+        avg_min - минимальная средняя зарплата (учитывает только зп 'от'),
+        avg_max - максимальная средняя зарплата (учитывает диапазон зп 'от/до'
+        """
+        return self.__execute_query("""
+        SELECT round(AVG(salary_from),2) AS avg_min, round(AVG(salary_from+salary_to),2) AS avg_max 
+        FROM vacancies
         """)
 
     def get_vacancies_with_higher_salary(self):
@@ -62,14 +72,13 @@ class DBManager:
 
     def get_vacancies_with_keyword(self, keyword):
         '''получает список всех вакансий, в названии которых содержатся переданные в метод слова'''
-        new_list_vacancies = []
-        vacancies = self.__execute_query("""
+        return self.__execute_query(f"""
         SELECT employers.name AS company_name, vacancies.name AS vac_name,
         vacancies.url, vacancies.salary_from, vacancies.salary_to
         FROM employers
         LEFT JOIN vacancies ON vacancies.employer=employers.id
+        WHERE LOWER(employers.name) LIKE LOWER('%{keyword}%') or 
+        LOWER(vacancies.name) LIKE LOWER('%{keyword}%')
         """)
-        for vac in vacancies:
-            if keyword.lower() in vac[1].lower():
-                new_list_vacancies.append(vac)
-        return new_list_vacancies
+
+
